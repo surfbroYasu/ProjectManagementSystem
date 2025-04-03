2216e9a516f77830152a874c16ec01e3ea2034f9
Yasufumi Misono <yasufumimisono@gmail.com>
2025-04-03 12:13:16 +0900

    確認：テンプレート、メッセージプロパティー、コントローラーの配置および動作確認

--- src/main/java/com/example/projectmanagement/modules/database/DatabaseSettingController.java
+++ src/main/java/com/example/projectmanagement/modules/database/DatabaseSettingController.java
@@ -1,16 +1,19 @@
 package com.example.projectmanagement.modules.database;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/database")
 public class DatabaseSettingController {
+	
+	private static final String TEMPLATE_ROOT="pages/";
 
 	@GetMapping("/hello")
-	public String testRendering() {
-		System.out.println("here!!!!!!!!!!!!!!!!!!!!!!!");
-		return "todo";
+	public String testRendering(Model model) {
+		model.addAttribute("title", "title.db.top");
+		return "test";
 	}
 }

e1fc6bb232f096980b49b254e760e0f620dea7ea
Yasufumi Misono <yasufumimisono@gmail.com>
2025-07-02 16:42:08 +0900

    追加：履歴機能のベースロジックを追加

--- src/main/java/com/example/projectmanagement/persistence/history/HistoryCommentMapper.java
+++ src/main/java/com/example/projectmanagement/persistence/history/HistoryCommentMapper.java
@@ -6,6 +6,9 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.example.projectmanagement.history.datastructures.entity.HistoryCommentEntity;
 
+/**
+ * このクラスは履歴機能のコメントのデータ操作（コメントのドメイン操作、コメントの取得）を行う
+ */
 @Mapper
 public interface HistoryCommentMapper {
 
--- src/main/java/com/example/projectmanagement/persistence/history/HistoryLocatorMapper.java
+++ src/main/java/com/example/projectmanagement/persistence/history/HistoryLocatorMapper.java
@@ -5,11 +5,28 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.example.projectmanagement.history.datastructures.entity.HistoryGroupEntity;
+import com.example.projectmanagement.history.datastructures.entity.UpdateHistoryEntity;
 
 @Mapper
 public interface HistoryLocatorMapper {
 
-	public HistoryGroupEntity findById(int id);
-	public List<HistoryGroupEntity> findByIdList(List<Integer>list);
+	/**
+	 * HistoryGroupのIdからHistoryGroupを見つけます。
+	 * @param id
+	 * @return
+	 */
+	public HistoryGroupEntity findHistoryGroupById(int id);
+	
+	/**
+	 * HistoryGroupのIdリストからHistoryGroupsを見つけリストを返却します。
+	 * @param list<Integer> idList
+	 * @return list<HisttoryGroypEntity>
+	 */
+	public List<HistoryGroupEntity> findHistoryGroupByIdList(List<Integer>list);
+	
+	
+	public List<UpdateHistoryEntity> getLastFiveUpdates(int groupId);
+	
+	public List<UpdateHistoryEntity> getAllUpdates(int groupId);
 	
 }
